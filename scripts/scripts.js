102e18c94c909b1385790a08bceace855f2a4ace
Gilles Kagarama <kagaramag@gmail.com>
2018-11-01 23:01:01 +0200

    UI improvement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -10,3 +10,13 @@ function closeSidebar() {
     m.style.display = "none";
     
 }
+
+window.addEventListener('scroll', function() {
+    var doc = document.documentElement;
+    var left = (window.pageXOffset || doc.scrollLeft) - (doc.clientLeft || 0);
+    var top = (window.pageYOffset || doc.scrollTop)  - (doc.clientTop || 0);
+    var val = document.getElementsByClassName("on-scroll-shadow");
+    if(top > 5){
+    val.style.background = "red";
+    }
+});
