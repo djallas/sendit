abadfd7acf9072680bbed912954fa4f1fdd50e5f
Gilles Kagarama <kagaramag@gmail.com>
2018-11-01 22:21:02 +0200

    sidebar menu, added google map, change logo, added new color, added favicon

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+// switch nav bar
+function toggleSidebar() {
+    var m = document.getElementById("sidebar");
+    m.style.display = "block";
+    
+}
+// close
+function closeSidebar() {
+    var m = document.getElementById("sidebar");
+    m.style.display = "none";
+    
+}
